3959e6df1b88905761888828dac8ea228c211c31
javendano <javendano@lean-tech.io>
2022-04-08 17:47:24 -0500

    Agregar archivos de proyecto.

---
+++ Assessment_BackEnd/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Assessment_BackEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = String.Empty;
+            string answer = String.Empty;
+
+            do
+            {
+                Console.Write("Enter the message: ");
+                message = Console.ReadLine();
+
+                message = String.Join("", message.Split('@', ',', '.', ';', '\'', '$', '#', '(', ')', '"', '=', '?', '¿', '¡', '!', '%'));
+
+                if (message == "")
+                    Console.WriteLine("NA");
+                else
+                    Console.WriteLine(message);
+                               
+                
+                Console.WriteLine("Do you want to enter another message? (Yes/No)");
+                answer = Console.ReadLine();
+            }
+            while (answer == "Yes");
+        }
+    }
+}
